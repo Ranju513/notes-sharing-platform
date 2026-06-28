3399563392cd867ed074adebd46ff6bcf13af39b
Ranjan Kumar <513ranjanronak@gmail.com>
2026-06-29 02:13:46 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-baseURL: "https://notes-sharing-platform-lofj.onrender.com/api"
+export default axios.create({
+  baseURL: "https://notes-sharing-platform-lofj.onrender.com/api",
 });
-
-export default api;
-
